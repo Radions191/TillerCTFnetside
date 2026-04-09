275a8d1271131262788e36d50c087f6c56f13e3d
Noah <noahdiakowski@gmail.com>
2026-04-09 16:46:10 +0200

    fix: fix routing between pages using react router

--- tillerCTFnetside/src/components/Navbar.tsx
+++ tillerCTFnetside/src/components/Navbar.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import { Link, Outlet } from "react-router";
 
 export default function Navbar() {
   return (
-    <div className="bg-green-900 text-black px-6 py-4 flex justify-center items-center">
-      <a href="#" className="hover:text-gray-200 text-lg font-bold">
-        Hall of Fame
-      </a>
-    </div>
+    <>
+      <div className="bg-green-900 text-black px-6 py-4 flex justify-center items-center">
+        <Link
+          to="/halloffame"
+          className="hover:text-gray-200 text-lg font-bold"
+        >
+          Hall of Fame
+        </Link>
+      </div>
+      <Outlet />
+    </>
   );
 }
