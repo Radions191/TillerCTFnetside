eaf64327861a6e5982612a99b979c5cb73494b21
Radions <radions.jevdokimovs@gmail.com>
2026-04-09 14:08:04 +0200

    Navbar

--- tillerCTFnetside/src/App.tsx
+++ tillerCTFnetside/src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
-function App() {
-  return <div>App</div>;
+export default function App() {
+  return (
+    <div className="bg-black min-h-screen text-white">
+      <Navbar />
+      <div className="p-6">
+        <div className="text-2xl font-bold flex justify-center">Tiller CTF</div>
+      </div>
+    </div>
+  );
 }
-
-export default App;
---
+++ tillerCTFnetside/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="bg-green-900 text-black px-6 py-4 flex justify-center items-center">
+      <a href="#" className="hover:text-gray-200 text-lg font-bold">
+        Hall of Fame
+      </a>
+    </div>
+  );
+}
