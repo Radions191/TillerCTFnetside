f2181255f43f8a6ddaaa6236e5c68d26e4836b20
Noah <noahdiakowski@gmail.com>
2026-04-09 17:05:29 +0200

    feat: basic logic and style of leaderboard

--- tillerCTFnetside/src/HallOfFame.tsx
+++ tillerCTFnetside/src/HallOfFame.tsx
@@ -1,5 +1,14 @@
+import LeaderBoard from "./components/LeaderBoard";
+import type { Players } from "./types/types";
+
 function HallOfFame() {
-  return <div>HallOfFame</div>;
+  const players: Players[] = [
+    { name: "placeholder1", wins: 1, avatar: "" },
+    { name: "placeholder2", wins: 2, avatar: "" },
+    { name: "placeholder3", wins: 3, avatar: "" },
+  ].sort((a, b) => a.wins - b.wins);
+
+  return <LeaderBoard players={players} />;
 }
 
 export default HallOfFame;
---
+++ tillerCTFnetside/src/types/types.ts
@@ -0,0 +1,5 @@
+export interface Players {
+  name: string;
+  wins: number;
+  avatar: string;
+}
