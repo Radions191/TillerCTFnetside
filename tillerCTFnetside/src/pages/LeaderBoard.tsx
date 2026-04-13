0c5ce9a941b350653843679c5e8ef5c724383aa0
Noah <noahdiakowski@gmail.com>
2026-04-13 20:27:04 +0200

    feat: add user statistics, implement figma styling in halloffame and leaderboard

--- tillerCTFnetside/src/pages/LeaderBoard.tsx
+++ tillerCTFnetside/src/pages/LeaderBoard.tsx
@@ -1,79 +1,104 @@
-import type { Players } from "../types/types";
+import type { CombinedPlayer, Players } from "../types/types";
 import PlayerCard from "../components/PlayerCard";
 import { Box, Typography } from "@mui/material";
-import fetchUsers from "../hooks/fetchUsers";
 import { useState, useEffect } from "react";
+import fetchApiData from "../hooks/fetchUsers";
 
 function LeaderBoard() {
-  const [players, setPlayers] = useState<Players[]>([]);
+  const [players, setPlayers] = useState<CombinedPlayer[]>([]);
   useEffect(() => {
     async function loadPlayers() {
-      const data = await fetchUsers();
-      setPlayers(data);
+      const userBasicData = await fetchApiData("users");
+      const userStatsData = await fetchApiData("stats");
+
+      const stats = userStatsData.scoreboard;
+
+      const merged: CombinedPlayer[] = userBasicData.map((user: Players) => {
+        const match = stats.find((stat: any) => stat.id === user.id);
+        return {
+          ...user,
+          attempts: match?.attempts ?? [],
+          solves: match?.solves ?? [],
+          place: match?.place ?? 0,
+          score: match?.score ?? 0,
+        };
+      });
+
+      setPlayers(merged);
     }
 
     loadPlayers();
   }, []);
 
-  const sortedPlayers = players.sort((a, b) => b.wins - a.wins);
+  const sortedPlayers = [...players].sort((a, b) => b.score - a.score);
   const topPlayers = sortedPlayers.slice(0, 12);
 
   const columns = 3;
 
   const sameWin: Players[] = [];
-  if (topPlayers.length > 1 && topPlayers[0].wins === topPlayers[1].wins) {
+  if (topPlayers.length > 1 && topPlayers[0].score === topPlayers[1].score) {
     sameWin.push(topPlayers[0], topPlayers[1]);
   }
 
-  if (!players || players.length <= 0) {
-    return (
-      <div className="flex justify-center min-h-[calc(100vh-60px)] pt-30 p-10 bg-slate-950">
-        <h1 className="bg-white p-5 rounded-2xl h-fit border-b-sky-950 border-4 text-2xl font-bold">
-          Looks like youre early! The CTF hasn't started yet..
-        </h1>
-      </div>
-    );
-  }
+  const totalFails = players.reduce((sum, player) => {
+    return sum + (player.attempts?.length || 0);
+  }, 0);
+
+  const mostChaotic = players
+    .slice()
+    .sort((a, b) => (b.attempts?.length || 0) - (a.attempts?.length || 0))[0];
+  console.log(mostChaotic);
 
   return (
-    <div className="flex flex-col w-full min-h-[calc(100vh-60px)] pt-5 items-center bg-slate-950 text-white">
-      <h1 className="text-2xl font-extrabold text-center mb-5">
-        CTF LEADERBOARD
-      </h1>
+    <div className="relative min-h-[calc(100vh-60px)] w-full flex flex-col items-center py-8 bg-[url('/hallOfFameBG.png')] bg-cover text-white">
+      <div className="absolute inset-0 bg-black/70 pointer-events-none" />
+      <div className="relative flex flex-col z-10 items-center w-full">
+        {!players ||
+          (players.length <= 0 ? (
+            <h1 className="bg-white p-5 rounded-2xl h-fit border-b-sky-950 border-4 text-2xl font-bold">
+              Looks like youre early! The CTF hasn't started yet..
+            </h1>
+          ) : (
+            <h1 className="text-2xl font-extrabold text-center text-white">
+              CTF LEADERBOARD
+            </h1>
+          ))}
 
-      <Box
-        sx={{
-          display: "grid",
-          gridTemplateColumns: {
-            xs: "1fr",
-            sm: `repeat(${columns - 1}, 1fr)`,
-            md: `repeat(${columns}, 1fr)`,
-          },
-          gap: 2,
-          px: { xs: 2, sm: 5, md: 10, lg: 15 },
-          mb: 2,
-        }}
-      >
-        {topPlayers.map((player, index) => (
-          <PlayerCard
-            key={index}
-            player={player}
-            index={index}
-            sameWin={sameWin}
-          />
-        ))}
-      </Box>
-
-      {players.length > 10 && (
-        <Typography
+        <Typography>Totale fails: ❌ {totalFails}</Typography>
+        {mostChaotic && (
+          <Typography sx={{ marginBottom: 4 }}>
+            Bruteforce-kongen: 💀 {mostChaotic.name.split(" ")[0]} med {mostChaotic.attempts.length} fails
+          </Typography>
+        )}
+        <Box
           sx={{
-            opacity: 0.8,
-            mb: { xs: 5, md: 0 },
+            display: "grid",
+            gridTemplateColumns: {
+              xs: "1fr",
+              sm: `repeat(${columns - 1}, 1fr)`,
+              md: `repeat(${columns}, 1fr)`,
+            },
+            gap: 2,
+            px: { xs: 2, sm: 5, md: 10, lg: 15 },
+            mb: 2,
           }}
         >
-          + {players.length - 10} more players
-        </Typography>
-      )}
+          {topPlayers.map((player, index) => (
+            <PlayerCard
+              key={index}
+              player={player}
+              index={index}
+              sameWin={sameWin}
+            />
+          ))}
+        </Box>
+
+        {players.length > 10 && (
+          <h3 className="opacity-80 xs:mb-5 md:mb-0">
+            + {players.length - 10} more players
+          </h3>
+        )}
+      </div>
     </div>
   );
 }
