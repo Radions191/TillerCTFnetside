43d17867deaf84bc35e9c4056c1309aa63d4b9a1
Noah <noahdiakowski@gmail.com>
2026-04-13 13:53:14 +0200

    style:winner cards add profile images

--- tillerCTFnetside/src/components/WinnerCard.tsx
+++ tillerCTFnetside/src/components/WinnerCard.tsx
@@ -1,35 +1,61 @@
-import { Card, CardContent, Typography } from "@mui/material";
+import { Card, CardMedia, CardContent, Typography } from "@mui/material";
 
-function WinnerCard({ name, year }: { name: string; year: number }) {
+function WinnerCard({
+  name,
+  classAttend,
+  year,
+  image,
+}: {
+  name: string;
+  classAttend: string;
+  year: number;
+  image: string;
+}) {
   return (
     <Card
       sx={{
         display: "flex",
-        width: "25%",
+        flexDirection: "column",
+        alignItems: "center",
+        width: 370,
+        height: 300,
+        border: "1px solid",
+        borderRadius: 3,
+        bgcolor: "#C3FFD1",
       }}
     >
+      <CardMedia
+        component="img"
+        image={image}
+        alt={`Profile picture of ${name}`}
+        sx={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          padding: 1,
+          borderRadius: 3,
+        }}
+      />
+
       <CardContent
         sx={{
-          flex: 1,
           display: "flex",
           flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
+          padding: 1.5,
+          textAlign: "center",
         }}
       >
         <Typography
-          variant="subtitle1"
           sx={{
             fontWeight: "bold",
-            fontSize: { xs: "0.9rem", sm: "1rem", lg: "1.2rem" },
-            textAlign: "center",
+            fontSize: "1.3rem",
           }}
         >
           {name}
         </Typography>
 
-        <Typography variant="body1" sx={{ textAlign: "center" }}>
-          👑{year}
+        <Typography sx={{ fontSize: "0.9rem", fontWeight:600 }}>
+          {classAttend} 👑 {year}
         </Typography>
       </CardContent>
     </Card>
--- tillerCTFnetside/src/pages/HallOfFame.tsx
+++ tillerCTFnetside/src/pages/HallOfFame.tsx
@@ -1,13 +1,59 @@
 import { Stack } from "@mui/material";
-import WinnerCard from "../components/WinnerCard";
+import ProfileCard from "../components/WinnerCard";
+import defaultPic from "../assets/vite.svg";
+import adrianPic from "../assets/adrian.png";
+import finnPic from "../assets/finn.png";
 
 function HallOfFame() {
   return (
-    <Stack sx={{ gap: 3, padding: 5 }}>
-      <WinnerCard name={"Finn Galloway McCann"} year={2025} />
-      <WinnerCard name={"Adrian Risvik"} year={2024} />
-      <WinnerCard name={"Sigurd Strand Osen"} year={2023} />
-    </Stack>
+    <div className="relative min-h-[calc(100vh-60px)] w-full flex flex-col items-center py-8 bg-[#062249] overflow-hidden">
+      <div className="absolute inset-0 bg-black/40 pointer-events-none" />{/* overlay */}
+      <div className="relative z-10 flex flex-col items-center w-full">
+        <h1 className="text-2xl font-semibold mb-2 text-white">
+          Årets Vinner:
+        </h1>
+
+        <div className="flex justify-center mb-15 w-1/3">
+          <ProfileCard
+            name="???"
+            classAttend="Sikkert Tiller"
+            year={2026}
+            image={defaultPic}
+          />
+        </div>
+
+        <Stack
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            gap: 4,
+            width: "100%",
+            height: "50%",
+            paddingX: 10,
+          }}
+        >
+          <ProfileCard
+            name="Finn Galloway McCann"
+            classAttend="CHA IT"
+            year={2025}
+            image={finnPic}
+          />
+          <ProfileCard
+            name="Adrian Risvik"
+            classAttend="2IT"
+            year={2024}
+            image={adrianPic}
+          />
+          <ProfileCard
+            name="Sigurd Strand Osen"
+            classAttend="1ID"
+            year={2023}
+            image={defaultPic}
+          />
+        </Stack>
+      </div>
+    </div>
   );
 }
 
