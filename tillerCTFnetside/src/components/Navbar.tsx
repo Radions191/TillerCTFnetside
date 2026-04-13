30c230470d5943722988b088f5838fad13cef316
Radions <radions.jevdokimovs@gmail.com>
2026-04-13 14:14:47 +0200

    changed background image, made navbar more flexible

--- tillerCTFnetside/src/components/Navbar.tsx
+++ tillerCTFnetside/src/components/Navbar.tsx
@@ -3,17 +3,23 @@ import { Link, Outlet } from "react-router";
 export default function Navbar() {
   return (
     <>
-      <div className=" bg-black text-white px-6 py-4 flex justify-center items-center">
+      <div className="bg-black text-white px-6 py-4 flex justify-center items-center gap-10">
         <Link to="/" className="hover:text-green-500 text-lg font-bold">
-          Home
+          Capture the Flag
         </Link>
+
         <Link
-          to="/halloffame"
+          to="/leaderboard"
           className="hover:text-green-500 text-lg font-bold"
         >
           Hall of Fame
         </Link>
+
+        <a href="/#om-oss" className="hover:text-green-500 text-lg font-bold">
+          Om oss
+        </a>
       </div>
+
       <Outlet />
     </>
   );
